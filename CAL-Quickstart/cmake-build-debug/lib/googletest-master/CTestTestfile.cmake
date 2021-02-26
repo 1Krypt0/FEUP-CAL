d715671ff8d71946b0d7f61b6e35e67872b7d528
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-02-26 23:31:10 +0000

    Made exercise 2 somewhere else, will need to add it later in here

--- CAL-Quickstart/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krypt0/Desktop/Uni/Second-Year/Second-Semester/CAL/CAL-Quickstart/lib/googletest-master
-# Build directory: /home/krypt0/Desktop/Uni/Second-Year/Second-Semester/CAL/CAL-Quickstart/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
